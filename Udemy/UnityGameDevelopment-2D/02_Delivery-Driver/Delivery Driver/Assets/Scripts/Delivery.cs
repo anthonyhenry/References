8472c3282627ce8c375b201391f61c6a9224068e
anthonyhenry <anthonyh1497@gmail.com>
2022-09-14 23:15:59 -0700

    Added boolean logic for picking up packages

--- Udemy/UnityGameDevelopment-2D/02_Delivery-Driver/Delivery Driver/Assets/Scripts/Delivery.cs
+++ Udemy/UnityGameDevelopment-2D/02_Delivery-Driver/Delivery Driver/Assets/Scripts/Delivery.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage = false;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Package")
         {
-            Debug.Log("Package out for delivery!");
+            hasPackage = true;
         }
-        else if(other.tag == "Customer")
+        else if(other.tag == "Customer" && hasPackage == true)
         {
             Debug.Log("Package Delivered!");
+            hasPackage = false;   
         }
     }
 }
