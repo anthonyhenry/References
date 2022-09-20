5629bdb1fad74aa3e5fd4c7c4155a5c455910376
anthonyhenry <anthonyh1497@gmail.com>
2022-09-19 20:59:09 -0700

    Destroy Packages and Change Car color

--- Udemy/UnityGameDevelopment-2D/02_Delivery-Driver/Delivery Driver/Assets/Scripts/Delivery.cs
+++ Udemy/UnityGameDevelopment-2D/02_Delivery-Driver/Delivery Driver/Assets/Scripts/Delivery.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     bool hasPackage = false;
+    [SerializeField] float destroyDelay = 0.5f;
+    SpriteRenderer spriteRenderer;
+    Color32 defaultColor;
+
+    void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        defaultColor = spriteRenderer.color;
+    }
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Package")
+        if(other.tag == "Package" && hasPackage == false)
         {
+            //Change the car's color to be the same as the package just picked up
+            spriteRenderer.color = other.GetComponent<SpriteRenderer>().color;
+            //Destroy the package
+            Destroy(other.gameObject, destroyDelay);
+            //Set the hasPackage flag
             hasPackage = true;
         }
         else if(other.tag == "Customer" && hasPackage == true)
         {
-            Debug.Log("Package Delivered!");
-            hasPackage = false;   
+            //Reset hasPackage
+            hasPackage = false;
+            //Reset the car's color   
+            spriteRenderer.color = defaultColor;
         }
     }
 }
