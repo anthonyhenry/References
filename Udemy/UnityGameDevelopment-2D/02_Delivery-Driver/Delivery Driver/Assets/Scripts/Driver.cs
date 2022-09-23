c391c66f3172f341ea4657405551c660daf14574
anthonyhenry <anthonyh1497@gmail.com>
2022-09-22 20:31:38 -0700

    Added Different Speeds

--- Udemy/UnityGameDevelopment-2D/02_Delivery-Driver/Delivery Driver/Assets/Scripts/Driver.cs
+++ Udemy/UnityGameDevelopment-2D/02_Delivery-Driver/Delivery Driver/Assets/Scripts/Driver.cs
@@ -5,12 +5,43 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 200f;
-    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float defaultSpeed = 10f;
+    float moveSpeed;
+    [SerializeField] float boostSpeed = 15f;
+    [SerializeField] float slowSpeed = 5f;
 
-    // Start is called before the first frame update
-    void Start()
+    void Start() 
     {
-        
+        moveSpeed = defaultSpeed;
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        moveSpeed = slowSpeed;
+        Debug.Log("Slowing");
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Boost")
+        {
+            Debug.Log("Boosting");
+            moveSpeed = boostSpeed;
+        }
+        else if(other.tag == "Obstacle")
+        {
+            moveSpeed = slowSpeed;
+            Debug.Log("Slowing");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) 
+    {
+        if(other.tag == "Obstacle")
+        {
+            moveSpeed =  defaultSpeed;
+            Debug.Log("Returning to default speed");
+        }    
     }
 
     // Update is called once per frame
