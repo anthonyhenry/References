cb878780daf35e718ec75483a8f698fbe796606c
anthonyhenry <anthonyh1497@gmail.com>
2023-05-22 19:33:08 -0700

    Week 2
    Update lecture-notes.py

---
+++ Python/CIS012-Introduction_to_Programming_Using_Python/02_WhyProgram/lecture-notes.py
@@ -0,0 +1,17 @@
+#If you search for IDLE on your computer you will open a Python Shell
+#Use exit() to end the shell program
+
+x = 5
+
+if x < 10:
+    print("Smaller")
+if x > 20:
+    print("Bigger")
+
+print("Finis")
+
+while x > 0:
+    print(x)
+    x = x - 1
+
+print("Blastoff!")
