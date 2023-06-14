4d36c09e528343cf6234f8a1f1d81b8e7587309c
anthonyhenry <anthonyh1497@gmail.com>
2023-06-13 20:04:42 -0700

    Setup Programming Fundamentals Folder

---
+++ Python/LinkedIn-Programming_Foundations_Fundamentals/01_VariablesAndDataTypes.py
@@ -0,0 +1,49 @@
+# Declaring Variables
+    # Varieble names can contian letters, numbers, and underscores
+    # Variable names cannot begin with a number
+    # Spaces are not allowed in variable names
+    # Variable names are case sensitive
+    # Variable names cannot be key words
+age = 36
+print(age)
+print(type(age))
+
+email = "john.doe@example.com"
+print(email)
+print(type(email))
+
+print("---")
+
+# You can freely change the type of a variable
+cookies = "sugar"
+print(cookies)
+print(type(cookies))
+
+cookies = 3
+print(cookies)
+print(type(cookies))
+
+Cookies = 3
+print(Cookies)
+print(cookies)
+
+print("---")
+
+first_name = "Jeff"
+print(first_name)
+
+first_Name = "Sara"
+print(first_name)
+
+print("---")
+
+#exponential operations
+result = 2**3
+print("2**3 =", result)
+
+print("---")
+
+# Floor division
+result = 5 // 3
+print(result)
+
