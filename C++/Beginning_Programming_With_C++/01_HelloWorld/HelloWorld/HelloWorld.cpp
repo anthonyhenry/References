ed0644f7ae98b0d5a37ebcb8da8eb5fab128fbf5
anthonyhenry <anthonyh1497@gmail.com>
2023-08-25 13:37:09 -0700

    Hello World

---
+++ C++/Beginning_Programming_With_C++/01_HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+// Program execution begins here
+int main()
+{
+	// Output the line "Hello World!" to command line
+	cout << "Hello World!" << endl;
+	// Terminate the program
+	return 0;
+}
