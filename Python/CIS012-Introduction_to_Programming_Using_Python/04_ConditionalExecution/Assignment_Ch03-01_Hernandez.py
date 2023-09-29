89c11f0fe4378e6b55f6a2f5572a7aa450149f0c
anthonyhenry <anthonyh1497@gmail.com>
2023-09-29 00:09:46 -0700

    Remove PCC CIS 012 (#8)

--- Python/CIS012-Introduction_to_Programming_Using_Python/04_ConditionalExecution/Assignment_Ch03-01_Hernandez.py
+++
@@ -1,8 +0,0 @@
-hours = input('Enter Hours: ')
-rate = input('Enter Rate: ')
-
-try:
-    pay = float(hours) * float(rate)
-    print('Pay:', pay)
-except:
-    print("Error: One or more inputs given was not valid.")
