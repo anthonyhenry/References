5c872f744485be2ca9d0aaa6f56db1ce7e9c19ce
anthonyhenry <anthonyh1497@gmail.com>
2023-09-29 01:01:08 -0700

    Update HelloWorld.cpp

--- C++/Beginning_Programming_With_C++/01_HelloWorld/HelloWorld.cpp
+++ C++/Beginning_Programming_With_C++/01_HelloWorld/HelloWorld.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// Program execution begins here
+// Program execution begins here. Every program must have a main() somewhere in it. Every program begins ath the open brace of the main() function and terminates at the return statment
 int main()
 {
     //Output "Hello World!" to the command line
