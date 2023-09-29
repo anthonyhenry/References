b095b20a6feea5c5a32b43811fa146b7741a953a
anthonyhenry <anthonyh1497@gmail.com>
2023-09-29 00:46:16 -0700

    Hello World VS Code
    Hello World project made in vs code plus some git ignore changes

---
+++ C++/Beginning_Programming_With_C++/01_HelloWorld/HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+// Program execution begins here
+int main()
+{
+    //Output "Hello World!" to the command line
+    cout << "Hello World!" << endl;
+    //Terminate the program
+    return 0;
+}
