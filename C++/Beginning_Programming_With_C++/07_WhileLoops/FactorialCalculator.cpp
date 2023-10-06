25559b71e36fd2d90a0ba1cf1863860b5eca83fb
anthonyhenry <anthonyh1497@gmail.com>
2023-10-05 17:26:42 -0700

    My attempt at factorial
    Update FactorialCalculator.cpp
    
    Update FactorialCalculator.cpp
    
    Update FactorialCalculator.cpp

---
+++ C++/Beginning_Programming_With_C++/07_WhileLoops/FactorialCalculator.cpp
@@ -0,0 +1,44 @@
+// Factorial Calculator - Calculates factorials using while loops
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Prompt the user for a number to calculate the factorial of
+    int factorial;
+    cout << "This program calculates factorial. \nEnter a number to take the factorial of: ";
+    cin >> factorial;
+
+    // Factorial of 0 is always 1
+    if(factorial == 0)
+    {
+        cout << "Factorial " << factorial << " is 1"; 
+    }
+    else
+    {
+        // Initialize variables for calculation
+        int multiplier = factorial - 1;
+        int multiplicand = factorial;
+        int total = 0; // This variable keeps track of the total factorial
+
+        while(multiplier > 1)
+        {
+            // Show the math
+            cout << multiplicand << " * " << multiplier << " = ";
+
+            total = multiplicand * multiplier;
+            multiplicand = total;
+            multiplier--;
+
+            cout << total << endl;
+        }
+
+        cout << "Factorial " << factorial << " is " << total << endl; 
+
+    }
+
+    system("PAUSE");
+    return 0;
+}
